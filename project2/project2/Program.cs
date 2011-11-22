fefdede6095e5576594cf93235faafe8e2feb435
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-11-22 17:59:24 +0100

    update to add usefull ref

--- project2/project2/Program.cs
+++ project2/project2/Program.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TeamCity.RunnerUsages;
 
 namespace project2
 {
   class Program
   {
     static void Main(string[] args)
-    {      
-
-
-
-
+    {
+      new Program545().ToString();
+      Console.Out.WriteLine(SuperFoo.Sup);
     }
   }
 }
